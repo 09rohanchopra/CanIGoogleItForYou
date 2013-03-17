78187dcedccf1dd267942d319a7497e4252df4a8
Rohan Chopra <09rohanchopra@gmail.com>
2013-03-17 14:57:06 +0530

    update 1.3
    hide keyboard when button pressed
    made copy and isonline methods
    isonline checks if device online or not

--- src/com/niner/cigifydona/MainActivity.java
+++ src/com/niner/cigifydona/MainActivity.java
@@ -6,6 +6,8 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.text.util.Linkify;
@@ -13,10 +15,12 @@ import android.view.KeyEvent;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import com.rosaloves.bitlyj.Url;
 
 public final class MainActivity extends Activity implements OnClickListener {
@@ -28,18 +32,12 @@ public final class MainActivity extends Activity implements OnClickListener {
 	static char[] tarr;
 	static int flag = 2;
 
-	public boolean onCreateOptionsMenu(android.view.Menu menu) {
-		// TODO Auto-generated method stub
-		super.onCreateOptionsMenu(menu);
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);		//to prevent screen from going into landscape mode 
-																					//and hence preventing the activity from restarting
+		this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT); 
+																				// to prevent screen from going into landscape mode
+																				// and hence preventing the activity from restarting
 		setContentView(R.layout.activity_main);
 		search = (Button) findViewById(R.id.bsearch);
 		share = (Button) findViewById(R.id.bshare);
@@ -52,6 +50,13 @@ public final class MainActivity extends Activity implements OnClickListener {
 		lucky.setOnClickListener(this);
 	}
 
+	public boolean onCreateOptionsMenu(android.view.Menu menu) {
+		// TODO Auto-generated method stub
+		super.onCreateOptionsMenu(menu);
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		// TODO Auto-generated method stub
@@ -60,12 +65,8 @@ public final class MainActivity extends Activity implements OnClickListener {
 			Intent abt = new Intent(MainActivity.this, AboutMe.class);
 			startActivity(abt);
 			break;
-		// case R.id.preferences:
-		// Intent p = new Intent(MainActivity.this, Preferences.class);
-		// startActivity(p);
-		// break;
 		case R.id.exit:
-			finish();			//if exit is selected from menu finish activity
+			finish(); // if exit is selected from menu finish activity
 			break;
 		}
 		return false;
@@ -74,55 +75,92 @@ public final class MainActivity extends Activity implements OnClickListener {
 	@Override
 	public void onClick(View arg0) {
 		// TODO Auto-generated method stub
-		if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.HONEYCOMB) {		//check if android version >3.0
-			StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()				//required for  generating bit.ly links
+		InputMethodManager imm = (InputMethodManager) getBaseContext()
+				.getSystemService(Context.INPUT_METHOD_SERVICE);
+
+		if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.HONEYCOMB) { // check
+																							// if
+																							// android
+																							// version
+																							// >3.0
+			StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder() // required
+																					// for
+																					// generating
+																					// bit.ly
+																					// links
 					.permitAll().build();
 			StrictMode.setThreadPolicy(policy);
 		}
 		String test = searchtext.getText().toString();
 		tarr = test.toCharArray();
-		makelink();
 		if (!test.matches("")) {
 			switch (arg0.getId()) {
-			case R.id.bsearch:					//do when search button pressed
+			case R.id.bsearch: // do when search button pressed
+				imm.hideSoftInputFromWindow(searchtext.getWindowToken(), 0);
+				makelink();
 				flag = 0;
 				link.setText("http://lmgtfy.com/?q=" + linkf);
-				Url url = as("9ninernine", "R_6602cf608d079310ec6da8a0e2802e51")
-						.call(shorten("http://lmgtfy.com/?q=" + linkf));
-				linkf = url.getShortUrl();
-				shorten.setText(linkf);
-				if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.HONEYCOMB) {		//check if android version >3.0
-					android.content.ClipboardManager clipboard = (android.content.ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
-					android.content.ClipData clip = android.content.ClipData
-							.newPlainText("text label", shorten.getText());
-					clipboard.setPrimaryClip(clip);											//copy link to clipboard
-					Toast.makeText(getApplicationContext(), 		//show toast
-							"Link Copied To Clipboard", Toast.LENGTH_SHORT)
-							.show();
+				if (isOnline()) {
+					try {
+						Url url = as("9ninernine",
+								"R_6602cf608d079310ec6da8a0e2802e51").call(
+								shorten("http://lmgtfy.com/?q=" + linkf));
+						linkf = url.getShortUrl();
+						shorten.setText(linkf);
+					} catch (Exception e) {
+
+					}
+					if (copy(true)) {
+						Toast.makeText(getApplicationContext(), // show toast
+								"Link Copied To Clipboard", Toast.LENGTH_SHORT)
+								.show();
+					}
+
+					shortu = linkf;
+				} else {
+					copy(false);
+					Toast.makeText(
+							getApplicationContext(),
+							"No Internet. Unable to generate Bit.ly link.\nNormal link copied to clipboard",
+							Toast.LENGTH_LONG).show();
+					shortu = link.getText().toString();
 				}
-				shortu = linkf;
+
 				break;
 			case R.id.blucky:
+				imm.hideSoftInputFromWindow(searchtext.getWindowToken(), 0);
+				makelink();
 				flag = 1;
 				link.setText("http://lmgtfy.com/?q=" + linkf + "&l=1");
-				Url urll = as("9ninernine",
-						"R_6602cf608d079310ec6da8a0e2802e51").call(
-						shorten("http://lmgtfy.com/?q=" + linkf + "&l=1"));
-				linkf = urll.getShortUrl();
-				shorten.setText(linkf);
-				if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.HONEYCOMB) {
-					android.content.ClipboardManager clipboard = (android.content.ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
-					android.content.ClipData clip = android.content.ClipData
-							.newPlainText("text label", shorten.getText());
-					clipboard.setPrimaryClip(clip);
-					Toast.makeText(getApplicationContext(),
-							"Link Copied To Clipboard", Toast.LENGTH_SHORT)
-							.show();
+				if (isOnline()) {
+					try {
+						Url urll = as("9ninernine",
+								"R_6602cf608d079310ec6da8a0e2802e51").call(
+								shorten("http://lmgtfy.com/?q=" + linkf
+										+ "&l=1"));
+						linkf = urll.getShortUrl();
+						shorten.setText(linkf);
+					} catch (Exception e) {
+
+					}
+					if (copy(true)) {
+						Toast.makeText(getApplicationContext(), // show toast
+								"Link Copied To Clipboard", Toast.LENGTH_SHORT)
+								.show();
+					}
+					shortu = linkf;
+				} else {
+					Toast.makeText(
+							getApplicationContext(),
+							"No Internet. Unable to generate Bit.ly link.\nNormal link copied to clipboard",
+							Toast.LENGTH_LONG).show();
+					copy(false);
+					shortu = link.getText().toString();
 				}
-				shortu = linkf;
 				break;
 			case R.id.bshare:
-				Intent intent = new Intent();									//for the share button
+				imm.hideSoftInputFromWindow(searchtext.getWindowToken(), 0);
+				Intent intent = new Intent(); // for the share button
 				intent.setAction(android.content.Intent.ACTION_SEND);
 				intent.setType("text/plain");
 				if (flag == 0) {
@@ -140,13 +178,40 @@ public final class MainActivity extends Activity implements OnClickListener {
 			Linkify.addLinks(shorten, Linkify.ALL);
 		} else {
 			Toast.makeText(getApplicationContext(),
-					"First enter a search query!", Toast.LENGTH_SHORT).show();			//if no search term
+					"First enter a search query!", Toast.LENGTH_SHORT).show(); // if
+																				// no
+																				// search
+																				// term
 		}
+	}
+
+	public boolean copy(boolean net) {
+		// TODO Auto-generated method stub
+		if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.HONEYCOMB) { // check
+																							// if
+																							// android
+																							// version
+																							// >3.0
+			android.content.ClipboardManager clipboard = (android.content.ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+			android.content.ClipData clip;
+			if (net) {
+				clip = android.content.ClipData.newPlainText("text label",
+						shorten.getText());
+			} else {
+				clip = android.content.ClipData.newPlainText("text label",
+						link.getText());
+			}
+			clipboard.setPrimaryClip(clip); // copy link to clipboard
+			return true;
+		} else
+			return false;
+
 	}
 
 	public static final void makelink() {
 		// TODO Auto-generated method stub
-
+		shorten.setText("");
+		link.setText("");
 		for (int i = 0; i < tarr.length; i++) {
 			if (tarr[i] == ' ')
 				tarr[i] = '+';
@@ -154,8 +219,18 @@ public final class MainActivity extends Activity implements OnClickListener {
 		}
 	}
 
+	public boolean isOnline() {
+		ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo netInfo = cm.getActiveNetworkInfo();
+		if (netInfo != null && netInfo.isConnectedOrConnecting()) {
+			return true;
+		}
+		return false;
+	}
+
 	public final boolean onKeyDown(int keyCode) {
-		if ((keyCode == KeyEvent.KEYCODE_BACK)) {		//finish when back key pressed
+		if ((keyCode == KeyEvent.KEYCODE_BACK)) { // finish when back key
+													// pressed
 			finish();
 		}
 		return super.onKeyDown(keyCode, null);
@@ -167,12 +242,4 @@ public final class MainActivity extends Activity implements OnClickListener {
 		super.onDestroy();
 		finish();
 	}
-
-	@Override
-	protected void onStop() {
-		// TODO Auto-generated method stub
-		super.onStop();
-		finish();
-	}
-
 }
